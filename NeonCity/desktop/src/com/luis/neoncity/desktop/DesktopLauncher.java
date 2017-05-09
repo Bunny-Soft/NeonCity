a85b8b8c1dbe2ea92e5aa417c1f3cba0cc91f1c7
SkyFir3 <najeraluis600@gmail.com>
2017-05-09 15:57:43 -0500

    Android compatibility
    added android compatibility

---
+++ NeonCity/desktop/src/com/luis/neoncity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.luis.neoncity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.luis.neoncity.NeonCity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NeonCity(), config);
+	}
+}
