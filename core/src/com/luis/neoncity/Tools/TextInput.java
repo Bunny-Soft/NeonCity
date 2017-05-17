1a3e274dd134000e0986b9fce2a8dbd434396f3e
Samantha <reformedcyan@gmail.com>
2017-05-17 13:42:20 -0500

    -Load button +ChangeCityName Button
    added TextInput class based on the TextInputListener interface

---
+++ core/src/com/luis/neoncity/Tools/TextInput.java
@@ -0,0 +1,34 @@
+package com.luis.neoncity.Tools;
+
+import com.badlogic.gdx.Input;
+
+/**
+ * Created by jz367071 on 5/17/2017.
+ */
+
+public class TextInput implements Input.TextInputListener {
+
+    String input = "null";
+
+    @Override
+    public void input (String text) {
+    input = text;
+    }
+
+    @Override
+    public void canceled () {
+    }
+
+    public String getInput()
+    {
+        return input;
+    }
+
+    public Boolean ifNull()
+    {
+        if(input.equals("null"))
+            return true;
+        else
+            return false;
+    }
+}
